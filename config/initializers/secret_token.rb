22f7c475a97567c1ab100e3ae431ba8a09e6e38c
Wanbok Choi <choi@wanbok.com>
2012-08-07 22:15:29 +0900

    Initialcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HospitalAggregator::Application.config.secret_token = 'ef33682a57e30dc0473df496efc3bc514c6268184340ed5563e1c9bad0b4181bfe92286e777ac94add6330f6475ec4e6fb6d5c21dd14e21b3a47427aabcae55d'
